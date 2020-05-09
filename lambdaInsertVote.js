f09784077b11a9558bfd1b3c125a2bb95265b797
KeeeG <KeeeG>
2020-05-09 16:58:17 +0200

    WIP: lambda 2 initial commit

---
+++ lambdaInsertVote.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+var config = require('./config.json');
+
+// DB Connections params
+var connection = mysql.createConnection({
+    host     : config.dbhost,
+    user     : config.dbuser,
+    password : config.dbpassword,
+    database : config.dbname,
+});
+
+exports.handler = (event, context, callback) => {
+    connection.query('INSERT INTO elections (cat_id) VALUES ("' + event.cat_id +'")', function (error, results, fields) {
+        if (error) {
+            connection.destroy();
+            throw error;
+        } else {
+            // connected!
+            console.log(results);
+            callback(error, results);
+            connection.end(function (err) { callback(err, results);});
+        }
+    });
+};
