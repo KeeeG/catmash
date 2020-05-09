e5788428075097eca588488cd090c0efebd23740
KeeeG <KeeeG>
2020-05-09 16:42:07 +0200

    WIP: adding the cat's images URL to the results

--- lambdaGetAllVotes.js
+++ lambdaGetAllVotes.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var config = require('./config.json');
 var fs = require("fs");
 var json = JSON.parse(fs.readFileSync('./cats.json'));
-var cats = [];
 var query;
 var queryResults = [];
+// DB Connections params
 var connection = mysql.createConnection({
     host     : config.dbhost,
     user     : config.dbuser,
@@ -14,23 +14,28 @@ var connection = mysql.createConnection({
 
 exports.handler = (event, context, callback) => {
   
+  // Parsing the JSON file, cat by cat  
   for (var i in json.images) {
-  	query = 'select IFNULL( cat_id, "'+ json.images[i].id +'" ) as cat_id, count(*) as votes from elections WHERE cat_id = "'+ json.images[i].id +'"';
+
+  	// Counting the cat's votes, adding the cat id and it's URL
+  	query = 'select IFNULL( cat_id, "'+ json.images[i].id +'" ) as cat_id, count(*) as votes, "'+ json.images[i].url +'" as url from elections WHERE cat_id = "'+ json.images[i].id +'"';
         connection.query(query , function (error, results, fields) {
             if (error) {
                 connection.destroy();
                 throw error;
             } else {
-                queryResults.push([ results[0].cat_id , JSON.stringify(results[0].votes)]);
+
+            	//pushing the results into one multidimentional array
+                queryResults.push([ results[0].cat_id , JSON.stringify(results[0].votes), results[0].url]);
             }
         });
         
     }
-    
-    // sorting the table
-    queryResults = queryResults.sort(function(a,b) {
+    // sorting the array
+    queryResults = queryResults.sort(function(a,b, c) {
         return b[1] - a[1];
-    });  
+    });
+    
     callback(null, queryResults);
     connection.end(function (err) { callback(err, queryResults);});
 };
